9bb8894dad48ac0b39e05e6d5c989b166f0a41f2
John Doe <contacto@marioanton.com>
2022-02-09 22:04:17 +0000

    metrics query typo

--- exp/services/recoverysigner/internal/account/db_store_count.go
+++ exp/services/recoverysigner/internal/account/db_store_count.go
@@ -2,7 +2,7 @@ package account
 
 func (s *DBStore) Count() (int, error) {
 	count := int(0)
-	err := s.DB.Get(&count, `SELECT COUNT(*) FROM accounts WHERE accounts.account_id IS NOT NULL`)
+	err := s.DB.Get(&count, `SELECT COUNT(*) FROM accounts WHERE account_id IS NOT NULL`)
 	if err != nil {
 		return 0, err
 	}
