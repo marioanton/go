74e7718b6720bccb533256ac5bd32a2028a52e8b
John Doe <contacto@marioanton.com>
2022-02-09 22:08:27 +0000

    removing metric query

--- exp/services/recoverysigner/internal/account/db_store_count.go
+++ exp/services/recoverysigner/internal/account/db_store_count.go
@@ -2,7 +2,7 @@ package account
 
 func (s *DBStore) Count() (int, error) {
 	count := int(0)
-	err := s.DB.Get(&count, `SELECT COUNT(*) FROM accounts WHERE account_id IS NOT NULL`)
+	err := s.DB.Get(&count, `SELECT COUNT(*) FROM accounts`)
 	if err != nil {
 		return 0, err
 	}
